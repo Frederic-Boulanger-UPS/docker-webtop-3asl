2ba4cf3f1bd4cc6faf363b1328033ace2d9619f1
Frédéric Boulanger <frederic.boulanger@universite-paris-saclay.fr>
2023-08-31 11:10:19 +0200

    Correction du script de lancement PowerShell

--- start-3asl.ps1
+++ start-3asl.ps1
@@ -6,6 +6,6 @@ $TAG="2023"
 $URL="localhost"
 $PORT="3000"
 $SPORT="3001"
-docker run --rm -d -p "${PORT}":"${PORT}" -v "${PWD}:/config:rw" --name "${IMAGE}-run" "${REPO}${IMAGE}:${TAG}"
+docker run --rm -d -p ${PORT}:${PORT} -v ${PWD}:/config:rw --name ${IMAGE}-run ${REPO}${IMAGE}:${TAG}
 Start-Sleep -s 5
 Start http://${URL}:${PORT}
